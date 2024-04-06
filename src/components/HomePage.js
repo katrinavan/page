00d901d85bcd17fb363e470aeb29e2188a60ee40
katrinavan <katrinavan@berkeley.edu>
2024-04-05 20:54:31 -0700

    ppppp

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './HomePage.css';
 
 const HomePage = () => {
-  return <div>Welcome to the HomePage</div>;
+  return (
+    <div className="home">
+      <h1>Welcome to the Blog!</h1>
+      <p>Our obejective of this blog is to showcase our favorite videos and a little bit of getting to know us!.</p>
+    </div>
+  );
 };
 
 export default HomePage;
