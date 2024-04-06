a418be057f79f679890f96f822e0fed902ac92fd
katrinavan <katrinavan@berkeley.edu>
2024-04-05 20:26:22 -0700

    pp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import AboutUsPage from './components/AboutUsPage';
+import FavoriteVideoPage from './components/FavoriteVideoPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about-us" component={AboutUsPage} />
+        <Route path="/favorite-videos" component={FavoriteVideoPage} />
+      </Switch>
+    </Router>
   );
 }
 
