be692c177064fa1528e92224b8c42db529296eaf
katrinavan <katrinavan@berkeley.edu>
2024-04-05 21:16:14 -0700

    fffoofw

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import HomePage from './components/HomePage';
-import AboutUsPage from './components/AboutUsPage';
-import FavoriteVideoPage from './components/FavoriteVideoPage'; // Make sure the file name matches this component name
-
+import NavBar from './components/NavBar';          // NavBar.js is present
+import HomePage from './components/HomePage';      // HomePage.js is present
+import AboutUsPage from './components/AboutUsPage';// AboutUsPage.js is present
+import FavoriteVideoPage from './components/FavoriteVideoPage'; // FavoriteVideoPage.js is present
 
 function App() {
   return (
     <Router>
-      <NavBar /> {/* Make sure this matches the import */}
+      <NavBar />
       <Routes>
-        <Route path="/" element={<HomePage />} /> {/* Removed exact */}
+        <Route path="/" element={<HomePage />} />
         <Route path="/about-us" element={<AboutUsPage />} />
         <Route path="/favorite-videos" element={<FavoriteVideoPage />} />
-  
       </Routes>
     </Router>
   );
