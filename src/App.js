38a4feca3f25ed6189b43b9206a644e78da378e8
katrinavan <katrinavan@berkeley.edu>
2024-04-05 21:10:55 -0700

    poopedr

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import AboutUsPage from './components/AboutUsPage';
-import FavoriteVideoPage from './components/FavoriteVideoPage';
-import './FadeTransition.css';
+import FavoriteVideoPage from './components/FavoriteVideoPage'; // Make sure the file name matches this component name
 
-const App = () => {
-  const location = useLocation(); // This is needed to make the transition work with React Router
 
+function App() {
   return (
     <Router>
-      <NavBar />
-      <TransitionGroup>
-        <CSSTransition
-          key={location.key}
-          classNames="fade"
-          timeout={250}
-        >
-          <Routes location={location}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about-us" element={<AboutUsPage />} />
-            <Route path="/favorite-videos" element={<FavoriteVideoPage />} />
-            {/* Add more routes as necessary */}
-          </Routes>
-        </CSSTransition>
-      </TransitionGroup>
+      <NavBar /> {/* Make sure this matches the import */}
+      <Routes>
+        <Route path="/" element={<HomePage />} /> {/* Removed exact */}
+        <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/favorite-videos" element={<FavoriteVideoPage />} />
+  
+      </Routes>
     </Router>
   );
-};
+}
 
 export default App;
